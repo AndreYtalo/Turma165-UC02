305c8f96e73906bfc105b99297fbaa773710a6a5
André Ytalo <andreytalo6@gmail.com>
2025-10-01 21:35:25 -0300

    java

---
+++ Aula17/JAVA/exemplo.js
@@ -0,0 +1,29 @@
+//Declarando varíaveis
+
+var nome = "André";
+
+console.log("Hello Word!");
+console.log("nome");
+nome = "André";
+console.log("nome");
+nome = 10;
+
+let idade = 25;
+
+const pi = 3.14;
+console.log("nome:", nome);
+
+console.log(`nome: ${nome}, idade: ${idade}, PI:${pi}`);
+
+
+
+
+let a = 10;
+let b = 3;
+let resultado = a * b;
+console.log(`soma: ${a+b}`);
+console.log(`O resultado de ${a} x ${b} é ${resultado}`);
+
+let comida = prompt("Informe o nome da sua comida favorita");
+console.log(comida);
+alert(`Sua comida favorita é ${comida}`);
