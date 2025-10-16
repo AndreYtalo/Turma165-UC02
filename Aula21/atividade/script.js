90bc0de5ca7cd7b11180831275d9b8b80fadbfdf
André Ytalo <andreytalo6@gmail.com>
2025-10-15 21:28:45 -0300

    atividade

--- Aula21/atividade/script.js
+++ Aula21/atividade/script.js
@@ -1,6 +1,6 @@
 function questão01() {
     let resultado = document.getElementById("resultado")
-    
+
     for (let i = 0; i <= 500; i++) {
         resultado.innerHTML += i + ",";
     }
@@ -10,8 +10,22 @@ function questão01() {
 
 function questão02() {
     const resultado02 = document.getElementById("resultado02");
-    
+
     for (let i = 0; i <= 100; i += 2) {
         resultado02.innerHTML += i + ",";
     }
+}
+
+
+
+function questao03() {
+    let value = Number(document.querySelector("#number").value);
+    let resultado03 = document.querySelector("#resultado03");
+    let resultado = "";
+
+    for (let i = 1; i <= 10; i++) {
+        resultado += `${value} x ${i} = ${value * i + "     "}`;
+    }
+
+    resultado03.textContent = resultado;
 }
---
+++ Aula22/script.js
@@ -0,0 +1,11 @@
+  let meuArray = [1, "dois",true,{chaves:"valor"}];
+
+
+  console.log(meuArray[0]);//Saída:1
+  console.log(meuArray[1]);//Saída:dois
+  console.log(meuArray[2]);//Saída:true
+  console.log(meuArray[3]);//Saída:{chaves:"valor"}
+
+
+
+  let tamArray = meuArray.length
